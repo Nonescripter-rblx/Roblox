53d039440544e2a8261044bf1696b9e395af5750
Nonescripter. <nonescripter@gmail.com>
2025-07-12 12:52:14 +0900

    Secret Staycation
    Disable Anticheat.

---
+++ SecretStaycation.lua
@@ -0,0 +1,9 @@
+--[[
+  Made by NoneScripter
+  Secret Staycation
+  You can use it on your video as showcase, or anything. Just leave a credit.
+]]--
+
+game:GetService("Players").LocalPlayer.PlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();
+game:GetService("StarterPlayer").StarterPlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();
+game:GetService("Players").LocalPlayer.PlayerGui.Version.TextLabel.Text=game:GetService("Players").LocalPlayer.PlayerGui.Version.TextLabel.Text .. " NoAntiCheatv1";
