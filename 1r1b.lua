3e967b203b5c6a0d2d531c778d33a1d9d9fed07e
Nonescripter. <nonescripter@gmail.com>
2025-09-01 16:01:00 +0900

    1 robucc = 1 robucc

--- 1r1b.lua
+++ 1r1b.lua
@@ -1,3 +1,28 @@
+--[[
+  Made by NoneScripter
+  1 robucc = 1 blocc
+  Block Spawner
+]]--
+
+local UIS = game:GetService("UserInputService")
+game:GetService("Workspace").AntiWeaponZone:Destroy()
+game:GetService("Workspace").HumanOnlyDoor:Destroy()
+Game:GetService("ReplicatedStorage").Events.MessageClient:fireServer("Removed AntiWeaponZone and HumanOnlyDoor!")
+
+local function onKeyPress(i, e)
+    if not e then
+        if i.KeyCode == Enum.KeyCode.V then
+            game:GetService("ReplicatedStorage").Events.GiveVirus:FireServer()
+        end
+        else if i.KeyCode == Enum.KeyCode.C then
+            game:GetService("ReplicatedStorage").Events.GiveCure:FireServer()
+        end
+    end
+end
+
+UIS.InputBegan:Connect(onKeyPress)
+Game:GetService("ReplicatedStorage").Events.MessageClient:fireServer("Press V to get Virus, Press C to get Cure")
+
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
